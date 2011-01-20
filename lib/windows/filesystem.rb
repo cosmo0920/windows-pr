1f8f833f39aac7f8913fff1ae70390eb10f17671
Daniel Berger <djberg96@gmail.com>
2011-01-19 21:06:48 -0700

    Cosmetic updates for some modules.

--- lib/windows/filesystem.rb
+++ lib/windows/filesystem.rb
@@ -1,13 +1,13 @@
 require 'windows/api'
 
 module Windows
-   module FileSystem
-      API.auto_namespace = 'Windows::FileSystem'
-      API.auto_constant  = true
-      API.auto_method    = true
-      API.auto_unicode   = true
+  module FileSystem
+    API.auto_namespace = 'Windows::FileSystem'
+    API.auto_constant  = true
+    API.auto_method    = true
+    API.auto_unicode   = true
 
-      API.new('GetDiskFreeSpace', 'SPPPP', 'B')
-      API.new('GetDiskFreeSpaceEx', 'SPPP', 'B')
-   end
+    API.new('GetDiskFreeSpace', 'SPPPP', 'B')
+    API.new('GetDiskFreeSpaceEx', 'SPPP', 'B')
+  end
 end
