113067c65121d55d5740c0a1ff0b527f6cf080e3
Daniel Berger <djberg96@gmail.com>
2011-02-06 12:31:05 -0700

    Added most of the remaining WSA functions and some constants.

--- lib/windows/wsa.rb
+++ lib/windows/wsa.rb
@@ -7,6 +7,22 @@ module Windows
     API.auto_method    = true
     API.auto_unicode   = true
 
+    # Constants
+
+    WSA_FLAG_OVERLAPPED             = 0x01
+    WSA_FLAG_MULTIPOINT_C_ROOT      = 0x02
+    WSA_FLAG_MULTIPOINT_C_LEAF      = 0x04
+    WSA_FLAG_MULTIPOINT_D_ROOT      = 0x08
+    WSA_FLAG_MULTIPOINT_D_LEAF      = 0x10
+    WSA_FLAG_ACCESS_SYSTEM_SECURITY = 0x40
+    WSA_FLAG_NO_HANDLE_INHERIT      = 0x80
+
+    # RNRSERVICE_REGISTER   = 0x01
+    # RNRSERVICE_DEREGISTER = 0x02
+    # RNRSERVICE_DELETE     = 0x04
+    
+    # Functions
+
     API.new('WSAAccept', 'LPPKP', 'L', 'ws2_32')
     API.new('WSAAddressToString', 'PLPPP', 'I', 'ws2_32')
     API.new('WSAAsyncGetHostByAddr', 'LISIIPI', 'L', 'ws2_32')
@@ -23,6 +39,54 @@ module Windows
     API.new('WSAConnectByList', 'LPPPPPPP', 'B', 'ws2_32')
     API.new('WSAConnectByName', 'LPPPPPPP', 'B', 'ws2_32')
     API.new('WSACreateEvent', 'V', 'L', 'ws2_32')
+    API.new('WSADeleteSocketPeerTargetName', 'LPLPP', 'L', 'fwpuclnt')
+    API.new('WSADuplicateSocket', 'LLP', 'I', 'ws2_32')
+    API.new('WSAEnumNameSpaceProviders', 'PP', 'I', 'ws2_32')
+    API.new('WSAEnumNameSpaceProvidersEx', 'PP', 'I', 'ws2_32')
+    API.new('WSAEnumNetworkEvents', 'LLP', 'I', 'ws2_32')
+    API.new('WSAEnumProtocols', 'PPP', 'I', 'ws2_32')
+    API.new('WSAEventSelect', 'LLL', 'I', 'ws2_32')
+    #API.new('__WSAFDIsSet', 'LP', 'I', 'ws2_32')
+    API.new('WSAGetLastError', 'V', 'I', 'ws2_32')
+    API.new('WSAGetOverlappedResult', 'LPPIP', 'B', 'ws2_32')
+    API.new('WSAGetQOSByName', 'LPP', 'B', 'ws2_32')
+    API.new('WSAGetServiceClassInfo', 'PPPP', 'I', 'ws2_32')
+    API.new('WSAGetServiceClassNameByClassId', 'PPP', 'I', 'ws2_32')
+    API.new('WSAHtonl', 'LLP', 'I', 'ws2_32')
+    API.new('WSAHtons', 'LIP', 'I', 'ws2_32')
+    API.new('WSAImpersonateSocketPeer', 'LPL', 'I', 'fwpuclnt')
+    API.new('WSAInstallServiceClass', 'P', 'I', 'ws2_32')
+    API.new('WSAIoctl', 'LLPLPLPPP', 'I', 'ws2_32')
+    API.new('WSAJoinLeaf', 'LPIPPPPL', 'L', 'ws2_32')
+    API.new('WSALookupServiceBegin', 'PLP', 'I', 'ws2_32')
+    API.new('WSALookupServiceEnd', 'L', 'I', 'ws2_32')
+    API.new('WSALookupServiceNext', 'LLPP', 'I', 'ws2_32')
+    API.new('WSANSPIoctl', 'LLPLPLPP', 'I', 'ws2_32')
+    API.new('WSANtohl', 'LLP', 'I', 'ws2_32')
+    API.new('WSANtohs', 'LIP', 'I', 'ws2_32')
+    API.new('WSAPoll', 'PLI', 'I', 'ws2_32')
+    API.new('WSAProviderConfigChange', 'PPP', 'I', 'ws2_32')
+    API.new('WSAQuerySocketSecurity', 'LPLPPPP', 'I', 'fwpuclnt')
+    API.new('WSARecv', 'LPLPPPP', 'I', 'ws2_32')
+    API.new('WSARecvDisconnect', 'LP', 'I', 'ws2_32')
+    API.new('WSARecvEx', 'LPIP', 'I', 'mswsock')
+    API.new('WSARecvFrom', 'LPLPPPPPP', 'I', 'ws2_32')
+    #API.new('WSARecvMsg', 'LPPPP', 'I', 'mswsock')
+    API.new('WSARemoveServiceClass', 'P', 'I', 'ws2_32')
+    API.new('WSAResetEvent', 'L', 'B', 'ws2_32')
+    API.new('WSARevertImpersonation', 'V', 'I', 'fwpuclnt')
+    API.new('WSASend', 'LPLPLPP', 'I', 'ws2_32')
+    API.new('WSASendDisconnect', 'LP', 'I', 'ws2_32')
+    API.new('WSASendMsg', 'LPLPPP', 'I', 'ws2_32')
+    API.new('WSASendTo', 'LPLPLPIPP', 'I', 'ws2_32')
+    API.new('WSASetEvent', 'L', 'B', 'ws2_32')
+    API.new('WSASetLastError', 'I', 'V', 'ws2_32')
+    API.new('WSASetService', 'PIL', 'I', 'ws2_32')
+    API.new('WSASetSocketPeerTargetName', 'LPLPP', 'I', 'fwpuclnt')
+    API.new('WSASetSocketSecurity', 'LPLPP', 'I', 'fwpuclnt')
     API.new('WSAStartup', 'LP', 'I', 'ws2_32')
+    API.new('WSASocket', 'IIIPLL', 'L', 'ws2_32')
+    API.new('WSAStringToAddress', 'PIPPP', 'I', 'ws2_32')
+    API.new('WSAWaitForMultipleEvents', 'LPILI', 'L', 'ws2_32')
   end
 end
