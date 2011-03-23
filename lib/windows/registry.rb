c69976722bbdc0b887cc94187a90c8119d6ad472
Daniel Berger <djberg96@gmail.com>
2011-03-22 21:54:58 -0600

    KEY_CURRENT_USER corrected to HKEY_CURRENT_USER in the Windows::Registry module.

--- lib/windows/registry.rb
+++ lib/windows/registry.rb
@@ -12,7 +12,7 @@ module Windows
     include Windows::File
     
     HKEY_CLASSES_ROOT        = 0x80000000
-    KEY_CURRENT_USER         = 0x80000001
+    HKEY_CURRENT_USER        = 0x80000001
     HKEY_LOCAL_MACHINE       = 0x80000002
     HKEY_USERS               = 0x80000003
     HKEY_PERFORMANCE_DATA    = 0x80000004
