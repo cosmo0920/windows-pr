242474fd8a4b14221e82348fb72a52886713d4a9
Daniel Berger <djberg96@gmail.com>
2012-03-28 08:11:12 -0600

    Converted the Windows::MSVCRT::Directory module to ffi and udpated its tests.

--- lib/windows/msvcrt/directory.rb
+++ lib/windows/msvcrt/directory.rb
@@ -1,31 +1,29 @@
-require 'windows/api'
+require 'ffi'
 
 module Windows
   module MSVCRT
     module Directory
-      API.auto_namespace = 'Windows::MSVCRT::Directory'
-      API.auto_method    = true
-      API.auto_constant  = true
-      API.auto_unicode   = false 
+      extend FFI::Library
+      ffi_lib 'msvcrt'
 
-      private
-
-      API.new('_chdir', 'S', 'I', MSVCRT_DLL)
-      API.new('_wchdir', 'S', 'I', MSVCRT_DLL)
-      API.new('_chdrive', 'I', 'I', MSVCRT_DLL)
-      API.new('_getcwd', 'PI', 'P', MSVCRT_DLL)
-      API.new('_wgetcwd', 'PI', 'P', MSVCRT_DLL)
-      API.new('_getdcwd', 'IPI', 'P', MSVCRT_DLL)
-      API.new('_wgetdcwd', 'IPI', 'P', MSVCRT_DLL)
-      API.new('_getdiskfree', 'IP', 'I', MSVCRT_DLL)
-      API.new('_getdrive', 'V', 'I', MSVCRT_DLL)
-      API.new('_getdrives', 'V', 'L', MSVCRT_DLL)
-      API.new('_mkdir', 'S', 'I', MSVCRT_DLL)
-      API.new('_wmkdir', 'S', 'I', MSVCRT_DLL)
-      API.new('_rmdir', 'S', 'I', MSVCRT_DLL)
-      API.new('_wrmdir', 'S', 'I', MSVCRT_DLL)
-      API.new('_searchenv', 'SSP', 'V', MSVCRT_DLL)
-      API.new('_wsearchenv', 'SSP', 'V', MSVCRT_DLL)
+      attach_function :chdir, :_chdir, [:string], :int
+      attach_function :wchdir, :_wchdir, [:string], :int
+      attach_function :chdrive, :_chdrive, [:int], :int
+      attach_function :getcwd, :_getcwd, [:pointer, :int], :pointer
+      attach_function :wgetcwd, :_wgetcwd, [:pointer, :int], :pointer
+      attach_function :getdcwd, :_getdcwd, [:int, :pointer, :int], :pointer
+      attach_function :wgetdcwd, :_wgetdcwd, [:int, :pointer, :int], :pointer
+      attach_function :getdiskfree, :_getdiskfree, [:ulong, :pointer], :ulong
+      attach_function :getdrive, :_getdrive, [], :int
+      attach_function :getdrives, :_getdrives, [], :ulong
+      attach_function :mkdir, :_mkdir, [:string], :int
+      attach_function :wmkdir, :_wmkdir, [:string], :int
+      attach_function :rmdir, :_rmdir, [:string], :int
+      attach_function :wrmdir, :_wrmdir, [:string], :int
+      attach_function :searchenv, :_searchenv, [:string, :string, :pointer], :void
+      attach_function :wsearchenv, :_wsearchenv, [:string, :string, :pointer], :void
+      attach_function :searchenv_s, :_searchenv_s, [:string, :string, :pointer, :size_t], :int
+      attach_function :wsearchenv_s, :_wsearchenv_s, [:string, :string, :pointer, :size_t], :int
     end
   end
 end
--- test/test_msvcrt_directory.rb
+++ test/test_msvcrt_directory.rb
@@ -1,11 +1,11 @@
 #####################################################################
-# tc_msvcrt_directory.rb
+# test_msvcrt_directory.rb
 #
 # Test case for the Windows::MSVCRT::File module.
 #####################################################################
 require 'windows/msvcrt/directory'
 require 'fileutils'
-require 'test/unit'
+require 'test-unit'
 
 class TC_Windows_MSVCRT_Directory < Test::Unit::TestCase
   include Windows::MSVCRT::Directory
@@ -16,24 +16,6 @@ class TC_Windows_MSVCRT_Directory < Test::Unit::TestCase
     @buf = 0.chr * 260
   end
 
-  def test_method_constants
-    assert_not_nil(Chdir)
-    assert_not_nil(Wchdir)
-    assert_not_nil(Chdrive)
-    assert_not_nil(Getcwd)
-    assert_not_nil(Wgetcwd)
-    assert_not_nil(Getdcwd)
-    assert_not_nil(Getdiskfree)
-    assert_not_nil(Getdrive)
-    assert_not_nil(Getdrives)
-    assert_not_nil(Mkdir)
-    assert_not_nil(Wmkdir)
-    assert_not_nil(Rmdir)
-    assert_not_nil(Wrmdir)
-    assert_not_nil(Searchenv)
-    assert_not_nil(Wsearchenv)
-  end
-   
   def test_chdir
     assert_nothing_raised{ chdir('..') }
     assert_equal(File.dirname(@pwd), Dir.pwd)
@@ -44,36 +26,36 @@ class TC_Windows_MSVCRT_Directory < Test::Unit::TestCase
   end
 
   def test_chdrive
-    assert(self.respond_to?(:chdrive, true))
+    assert_respond_to(self, :chdrive)
   end
 
   def test_getcwd
-    assert(self.respond_to?(:getcwd, true))
+    assert_respond_to(self, :getcwd)
   end
 
   def test_wgetcwd
-    assert(self.respond_to?(:wgetcwd, true))
+    assert_respond_to(self, :wgetcwd)
   end
 
   def test_getdcwd
-    assert(self.respond_to?(:getdcwd, true))
+    assert_respond_to(self, :getdcwd)
   end
 
   def test_wgetdcwd
-    assert(self.respond_to?(:wgetdcwd, true))
+    assert_respond_to(self, :wgetdcwd)
   end
 
   def test_mkdir
-    assert(self.respond_to?(:mkdir, true))
+    assert_respond_to(self, :mkdir)
     assert_nothing_raised{ mkdir(@dir) }
-    assert_equal(true, File.exists?(@dir))
+    assert_true(File.exists?(@dir))
   end
 
   def test_rmdir
     Dir.mkdir(@dir) unless File.exists?(@dir)
-    assert(self.respond_to?(:rmdir, true))
+    assert_respond_to(self, :rmdir)
     assert_nothing_raised{ rmdir(@dir) }
-    assert_equal(false, File.exists?(@dir))
+    assert_false(File.exists?(@dir))
   end
 
   def test_searchenv
@@ -82,9 +64,9 @@ class TC_Windows_MSVCRT_Directory < Test::Unit::TestCase
       "c:\\windows\\system32\\notepad.exe",
       "c:\\windows\\notepad.exe"
     ]
-    assert(self.respond_to?(:searchenv, true))
+    assert_respond_to(self, :searchenv)
     assert_nothing_raised{ searchenv("notepad.exe", "PATH", @buf) }
-    assert_equal(true, possible.include?(@buf.strip.downcase))
+    assert_true(possible.include?(@buf.strip.downcase))
   end
 
   def teardown
