f8a39a353a98e1d48b291c603cf388fee94979b1
Daniel Berger <djberg96@gmail.com>
2014-10-18 06:52:16 -0600

    Added a helper to maintain backwards compatibility with test-unit.

---
+++ test/helper.rb
@@ -0,0 +1,13 @@
+require 'minitest/unit'
+
+module Minitest::Assertions
+  def assert_not_nil(*args)
+    refute_nil(*args)
+  end
+
+  def assert_nothing_raised(*args)
+    yield
+  end
+
+  alias assert_raise assert_raises
+end
